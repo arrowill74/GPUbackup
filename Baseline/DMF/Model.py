b3b307c6a98d92434444d506a48271f8f255577d
Yuzen <arrowill74@gmail.com>
2020-05-04 20:07:33 +0800

    Baseline DMF chilli's edition

--- Baseline/DMF/Model.py
+++ Baseline/DMF/Model.py
@@ -22,15 +22,15 @@ def main():
     parser = argparse.ArgumentParser(description="Options")
 
     parser.add_argument('-dataName', action='store', dest='dataName', default='ml-1m')
-    parser.add_argument('-negNum', action='store', dest='negNum', default=7, type=int)
-    parser.add_argument('-userLayer', action='store', dest='userLayer', default=[50, 64]) #default=[512, 64])
-    parser.add_argument('-itemLayer', action='store', dest='itemLayer', default=[10, 64]) #default=[1024, 64])
+    # parser.add_argument('-negNum', action='store', dest='negNum', default=7, type=int)
+    parser.add_argument('-userLayer', action='store', dest='userLayer', default=[512, 64])
+    parser.add_argument('-itemLayer', action='store', dest='itemLayer', default=[1024, 64])
     # parser.add_argument('-reg', action='store', dest='reg', default=1e-3)
     parser.add_argument('-lr', action='store', dest='lr', default=0.0001)
-    parser.add_argument('-maxEpochs', action='store', dest='maxEpochs', default=50, type=int)#default=50, type=int)
-    parser.add_argument('-batchSize', action='store', dest='batchSize', default=25, type=int)#default=256, type=int)
+    parser.add_argument('-maxEpochs', action='store', dest='maxEpochs', default=20, type=int)#default=50, type=int)
+    parser.add_argument('-batchSize', action='store', dest='batchSize', default=256, type=int)
     parser.add_argument('-earlyStop', action='store', dest='earlyStop', default=5)
-    parser.add_argument('-checkPoint', action='store', dest='checkPoint', default='./checkPoint/')
+    # parser.add_argument('-checkPoint', action='store', dest='checkPoint', default='./checkPoint/')
     parser.add_argument('-topK', action='store', dest='topK', default=10)
 
     args = parser.parse_args()
@@ -50,10 +50,10 @@ class Model:
         self.train = self.dataSet.train
         self.test = self.dataSet.test
 
-        self.negNum = args.negNum
+        # self.negNum = args.negNum
         # self.testNeg = self.dataSet.getTestNeg(self.test, 99)
         # 第一個是positive 剩下31個是negtive
-        self.testNeg = self.dataSet.getTestNeg(self.test, 31) # [np.array(user), np.array(item)] (1078, 1078)
+        self.testNeg = self.dataSet.getTestNeg(self.test) # [np.array(user), np.array(item)] (1078, 1078)
         # print(self.testNeg[0].shape, self.testNeg[1].shape) # (1078, 32) (1078, 32)
         self.add_embedding_matrix()
 
